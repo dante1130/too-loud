f765da9003c3bac2173adbffce8234c62c50d577
dante1130 <andrewdante2@gmail.com>
2023-08-27 18:13:58 +0800

    Show window when volume threshold is met

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+from tkinter import messagebox
 import sounddevice as sd
 import numpy as np
 
 from queue import Queue
 
-
+VOLUME_THRESHOLD = 15
 audio_queue = Queue()
+was_too_loud = False
 
 
 def read_sound(indata, frames, time, status):
@@ -15,5 +17,9 @@ with sd.InputStream(callback=read_sound):
     while True:
         volume_norm = np.linalg.norm(audio_queue.get()) * 10
 
-        if volume_norm > 10:
-            print(f"Your volume is: {volume_norm}")
+        if volume_norm > VOLUME_THRESHOLD and not was_too_loud:
+            was_too_loud = True
+            messagebox.showwarning("You're too loud!",
+                                   f"Volume: {volume_norm}")
+        elif volume_norm < VOLUME_THRESHOLD and was_too_loud:
+            was_too_loud = False
